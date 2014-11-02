b22fe51176404043ad9820ad87be75f639a56852
blckshrk <alexand.bonhom@gmail.com>
2014-11-02 18:22:30 +0100

    Adds language selection support

--- controller.js
+++ controller.js
@@ -6,15 +6,18 @@ angular.module('ArteHack', [])
 
 	$scope.video_format = 'HBBTV';
 	$scope.video_quality = 'SQ';
+	$scope.video_lang = 'VF';
+
 	$scope.arte_url = '';
-	$scope.video_url = ''
+	$scope.video_url = '';
 
 
 	$scope.showURL = function () {
+		if (! $scope.arte_url ) {
+			return false;
+		}
 
 		var videoIdPattern = new RegExp(/\d*\-\d*/);
-
-		//$scope.video_url 
 		var videoId = videoIdPattern.exec($scope.arte_url)[0];
 
 		var urlJSON = $scope.ARTE_BASE_URL + videoId + '_PLUS7-F/' + $scope.video_format + '/' + $scope.video_quality + '.json';
@@ -23,20 +26,47 @@ angular.module('ArteHack', [])
         responsePromise.success(function(data, status, headers, config) {
         	var videosObj = data.videoJsonPlayer.VSR
         	
+        	var videoLangPattern = buildLangPattern($scope.video_lang);
+
         	for (var key in videosObj) {
         		if (!videosObj.hasOwnProperty(key)) {
         			continue;
         		}
 
-        		if (videosObj[key].versionCode == "VOF-STF") {
+        		if (videoLangPattern.test(videosObj[key].versionCode)) {
         			$scope.video_url = videosObj[key].url;
         			break;
         		}
         	}
+
+        	if (! $scope.video_url) {
+        		addAlert('Oops... I have not found the video in this language :-/');
+        	}
         });
 
         responsePromise.error(function(data, status, headers, config) {
-            alert('Oops... Something bad happened :(');
+            addAlert('Oops... Something really bad happened :-(');
         });
+	}
+
+	/* 
+	   Return a regex obj to match the correct libelle in the JSON file
+	   regarding to the selected language for the video.
+	 */
+	function buildLangPattern(lang) {
+		switch(lang) {
+			case 'VA':
+				return new RegExp(/^V[O]?A/);
+
+			case 'VF':
+				return new RegExp(/^V[O]?F/);
+		}
+	}
+
+
+	function addAlert(message) {
+		var escapedMessage = message.replace(/'/g, "\\\'");
+
+		$('#alert').append('<div class="alert alert-danger alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button><p>' + escapedMessage + '</p></div>');
 	}
 });
